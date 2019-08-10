d65238237ae150af7b12699e31ec9fde1a2732f2
Waga <wagaodongo@gmail.com>
2019-08-10 16:30:01 +0300

    question bank

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz.dart';
+
+Quiz quiz = Quiz();
 
 void main() => runApp(Quizzler());
 
@@ -25,21 +28,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(Icons.close, color: Colors.red)
-  ];
+  List<Widget> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-
-  List<bool> answers = [false, true, true];
   int questionNumber = 0;
 
   @override
@@ -54,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quiz.qbank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,7 +68,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = quiz.qbank[questionNumber].questionAnswer;
 
                 if (correctAnswer == true) {
                   print('You got one');
@@ -106,7 +96,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = quiz.qbank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                   print('You got one');
                 } else {
